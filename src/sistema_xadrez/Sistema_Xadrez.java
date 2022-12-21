036e0347e2c4e3d761abf6c9b3889db916da88c3
Pedro Bastos <pedrolsouza10@outlook.com>
2022-12-21 16:24:02 -0300

    Project created

---
+++ src/sistema_xadrez/Sistema_Xadrez.java
@@ -0,0 +1,13 @@
+package sistema_xadrez;
+
+/**
+ *
+ * @author pedro
+ */
+public class Sistema_Xadrez {
+
+    public static void main(String[] args) {
+        System.out.println("Ola");
+    }
+    
+}
