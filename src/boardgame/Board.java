0f43624d3eb736a57187479fee90822114bf7071
Pedro Bastos <pedrolsouza10@outlook.com>
2022-12-21 16:43:49 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,49 @@
+package boardgame;
+
+/**
+ *
+ * @author pedro
+ */
+public class Board {
+    private int rows;   //linhas
+    private int columns; //colunas
+    private Piece[][] pieces;
+    
+    public Board(int rows, int columns){
+        this.rows=rows;
+        this.columns=columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    /**
+     * @return the rows
+     */
+    public int getRows() {
+        return rows;
+    }
+
+    /**
+     * @param rows the rows to set
+     */
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    /**
+     * @return the columns
+     */
+    public int getColumns() {
+        return columns;
+    }
+
+    /**
+     * @param columns the columns to set
+     */
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+    
+    
+    
+    
+}
